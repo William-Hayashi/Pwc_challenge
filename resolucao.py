ee5509205e374ab0402d46f245a4b8172e0f4237
William-Hayashi <126543236+William-Hayashi@users.noreply.github.com>
2024-02-12 01:32:09 +0000

    second commit

--- resolucao.py
+++ resolucao.py
@@ -16,3 +16,10 @@ def processar_endereco(endereco):
 
     # Se o padrão não combinar, retorna uma mensagem de erro
     return {"erro": "Formato de endereço inválido"}
+
+    # Casos de teste
+print(processar_endereco("Miritiba   339"))  # {'nome_rua': 'Miritiba', 'numero_rua': '339'}
+print(processar_endereco("Babaçu   500"))  # {'nome_rua': 'Babaçu', 'numero_rua': '500'}
+print(processar_endereco("Cambuí   804B"))  # {'nome_rua': 'Cambuí', 'numero_rua': '804B'}
+print(processar_endereco("Rio Branco   23"))  # {'nome_rua': 'Rio Branco', 'numero_rua': '23'}
+print(processar_endereco("Quirino dos Santos   23 b"))  # {'nome_rua': 'Quirino dos Santos', 'numero_rua': '23 b'}
